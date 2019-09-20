2fdafa054e6df750627f5b73b2615f9670bebf53
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-09-20 10:48:48 +0200

    added more helpers for the pile of cards

--- game/Pile.ts
+++ game/Pile.ts
@@ -28,3 +28,25 @@ export function placeCardOnTop(pile: IPile, card: ICard): IPile {
 export function placePileOnTop(bottomPile: IPile, topPile: IPile): IPile {
   return new Pile(bottomPile.cards.concat(topPile.cards))
 }
+
+export function replaceCard(pile: IPile, cardToReplace: ICard, newCard: ICard): IPile {
+  const index = pile.cards.indexOf(cardToReplace)
+  if (index > -1) {
+    const patchedCards = pile.cards.slice()
+    patchedCards.splice(index, 1, newCard)
+    return new Pile(patchedCards)
+  }
+  return pile
+}
+
+export function shuffle(pile: IPile): IPile {
+  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+  const cards = pile.cards.slice()
+  for (let currentCardIndex = 0; currentCardIndex < cards.length - 1; currentCardIndex++) {
+    const randomCardIndex = Math.floor(Math.random() * (cards.length - currentCardIndex)) + currentCardIndex
+    const currentCard = cards[currentCardIndex]
+    cards[currentCardIndex] = cards[randomCardIndex]
+    cards[randomCardIndex] = currentCard
+  }
+  return new Pile(cards)
+}
