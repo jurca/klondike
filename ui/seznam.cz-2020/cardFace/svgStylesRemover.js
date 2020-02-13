cf137b40d6bd40f6933881c9ee163149f6bbd718
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-02-13 14:17:18 +0100

    Replace CSS styling with attributes and drop extraneous IDs for spades

---
+++ ui/seznam.cz-2020/cardFace/svgStylesRemover.js
@@ -0,0 +1,41 @@
+// Unfortunately, SVGO (https://jakearchibald.github.io/svgomg/) does not replace all style declarations with
+// attributes, and since SVG style declarations clash with each other when the SVG is used inline, we had to remove them
+// like this
+
+function rgbToHex(rgb) {
+  const parts = rgb.slice(4, -1).split(/\s*,\s*/).map(part => parseInt(part, 10).toString(16).padStart(2, '0'))
+  return `#${parts.join('')}`
+}
+
+function parseStyle(style) {
+  const rules = style.sheet.rules
+  const styling = {}
+  for (const {selectorText, style} of rules) {
+    if (!/^\.cls-\d+$/.test(selectorText) || style[0] !== 'fill') {
+      continue
+    }
+    styling[selectorText.substring(1)] = rgbToHex(style['fill'])
+  }
+  return styling
+}
+
+function replaceStyles(cardRoot) {
+  const styleNode = cardRoot.querySelector('style')
+  const style = parseStyle(styleNode)
+  for (const [className, fillColor] of Object.entries(style)) {
+    for (const node of cardRoot.querySelectorAll(`[class=${className}]`)) {
+      node.setAttribute('fill', fillColor)
+      node.removeAttribute('class')
+    }
+  }
+  styleNode.parentNode.removeChild(styleNode)
+}
+
+[...document.querySelectorAll('klondike-card style')].map(node => node.closest('klondike-card')).map(
+  card => (replaceStyles(card), card.querySelectorAll('svg')[1])
+)
+
+// The following snippet then removes all id attributes except the ones on <linearGradient> elements
+;[...document.querySelectorAll(['[id]'])].filter(node => node.tagName !== 'linearGradient').map(
+  node => (node.removeAttribute('id'), node.closest('svg'))
+)
