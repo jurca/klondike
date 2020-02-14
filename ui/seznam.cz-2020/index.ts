4e6731f306c694a01eb4ed95ad6374e6a97638e4
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-02-14 10:28:18 +0100

    Visual check of card backfaces

--- ui/seznam.cz-2020/index.ts
+++ ui/seznam.cz-2020/index.ts
@@ -1,6 +1,8 @@
 import {html, render} from 'lighterhtml'
 import {Color, Rank, Side} from '../../game/Card'
 import Card from './Card'
+import CardBackface from './CardBackface'
+import CardBackfaceStyle from './CardBackfaceStyle'
 
 const uiRoot = document.getElementById('app')!
 render(uiRoot, html`
@@ -10,5 +12,10 @@ render(uiRoot, html`
         html`<div style="margin: 0 10px">${Card({color, rank, side: Side.FACE})}</div>`,
       )}</li>`,
     )}
+    <li style="display: flex; padding: 10px 0">
+      ${Object.values(CardBackfaceStyle).map((style) =>
+        html`<div style="margin: 0 10px; width: 1em; height: 1.5em">${CardBackface(style)}</div>`,
+      )}
+    </li>
   </ul>
 `)
