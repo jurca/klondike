892fb583d6ad326ef54d8963c4b8431469681a86
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-03-06 16:07:32 +0100

    Fix issues with draggable being incorrectly cached

--- ui/seznam.cz-2020/draggable.ts
+++ ui/seznam.cz-2020/draggable.ts
@@ -3,22 +3,29 @@ import {html, neverland, useContext, useRef} from 'neverland'
 import styles from './draggable.css'
 import {DRAG_N_DROP_CONTEXT} from './DragNDrop'
 
-export default neverland(function draggable(content: unknown) {
-  const dragNDropContext = useContext(DRAG_N_DROP_CONTEXT)
-  const wrapperRef = useRef<HTMLElement>()
+const draggableWrapperCache = new WeakMap()
 
-  const isDragged = dragNDropContext.dragged === wrapperRef.current
-  const {draggedElementOriginalPosition, draggedElementPosition} = dragNDropContext
-  const deltaX = draggedElementPosition.x - draggedElementOriginalPosition.x
-  const deltaY = draggedElementPosition.y - draggedElementOriginalPosition.y
+export default function draggable(content: object) {
+  const wrapper = draggableWrapperCache.get(content) || neverland(() => {
+    const dragNDropContext = useContext(DRAG_N_DROP_CONTEXT)
+    const wrapperRef = useRef<HTMLElement>()
 
-  return html`
-    <ui-draggable
-      class=${classnames(styles.draggable, isDragged && styles.isDragged)}
-      style=${isDragged ? `transform: translateX(${deltaX}px) translateY(${deltaY}px)` : null}
-      ref=${wrapperRef}
-    >
-      ${content}
-    </ui-draggable>
-  `
-})
+    const isDragged = dragNDropContext.dragged === wrapperRef.current
+    const {draggedElementOriginalPosition, draggedElementPosition} = dragNDropContext
+    const deltaX = draggedElementPosition.x - draggedElementOriginalPosition.x
+    const deltaY = draggedElementPosition.y - draggedElementOriginalPosition.y
+
+    return html`
+      <ui-draggable
+        class=${classnames(styles.draggable, isDragged && styles.isDragged)}
+        style=${isDragged ? `transform: translateX(${deltaX}px) translateY(${deltaY}px)` : null}
+        ref=${wrapperRef}
+      >
+        ${content}
+      </ui-draggable>
+    `
+  })
+  draggableWrapperCache.set(content, wrapper)
+
+  return wrapper()
+}
