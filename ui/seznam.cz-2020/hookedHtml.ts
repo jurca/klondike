90308d9cfceaffdc759ed892caaea6adce3881a3
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-03-06 13:40:46 +0100

    Add html.for a and svg.for to hooked HTML template tags to simplify use

--- ui/seznam.cz-2020/hookedHtml.ts
+++ ui/seznam.cz-2020/hookedHtml.ts
@@ -6,5 +6,15 @@ import {html as $html, svg as $svg} from 'lighterhtml'
 // lighterhtml no longer exposes the hook function in favor of using html.for()/svg.for() for this (see
 // https://github.com/WebReflection/lighterhtml#v2-breaking-changes--improvements).
 
-export const html = (...args: [TemplateStringsArray, ...unknown[]]) => $html.for(useRef(null), '').apply(null, args)
-export const svg = (...args: [TemplateStringsArray, ...unknown[]]) => $svg.for(useRef(null), '').apply(null, args)
+export const html = Object.assign(
+  (...args: [TemplateStringsArray, ...unknown[]]) => $html.for(useRef(null), '').apply(null, args),
+  {
+    for: $html.for,
+  },
+)
+export const svg = Object.assign(
+  (...args: [TemplateStringsArray, ...unknown[]]) => $svg.for(useRef(null), '').apply(null, args),
+  {
+    for: $svg.for,
+  },
+)
