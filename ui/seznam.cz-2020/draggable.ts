061c3fa8ea7dcbcc0f8a49023bd813dfe5eb7439
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-03-06 13:27:01 +0100

    Simplify the code of the draggable high-order component
    Now that hooks work with drag&drop as they should, the code can be cleaned up.

--- ui/seznam.cz-2020/draggable.ts
+++ ui/seznam.cz-2020/draggable.ts
@@ -1,34 +1,22 @@
-import {augmentor, useContext, useEffect, useRef} from 'dom-augmentor'
+import classnames from 'classnames'
+import {augmentor, useContext, useRef} from 'dom-augmentor'
 import {Hole} from 'lighterhtml'
 import styles from './draggable.css'
 import {DRAG_N_DROP_CONTEXT} from './DragNDrop'
 import {html} from './hookedHtml'
 
-export default augmentor(function draggable(content: Hole): Hole {
+export default augmentor(function draggable(content: Hole) {
   const dragNDropContext = useContext(DRAG_N_DROP_CONTEXT)
   const wrapperRef = useRef<HTMLElement>()
-  useEffect(() => {
-    const wrapperNode = wrapperRef.current
-    if (wrapperNode) {
-      // For some reason updating the attributes via lighterhtml does not work :(
-      if (dragNDropContext.dragged === wrapperNode) {
-        const {draggedElementOriginalPosition, draggedElementPosition} = dragNDropContext
-        const deltaX = draggedElementPosition.x - draggedElementOriginalPosition.x
-        const deltaY = draggedElementPosition.y - draggedElementOriginalPosition.y
-        wrapperNode.style.transform = `translateX(${deltaX}px) translateY(${deltaY}px)`
-        wrapperNode.classList.add(styles.isDragged)
-      } else if (wrapperNode.style.transform) {
-        wrapperNode.style.transform = ''
-        wrapperNode.classList.remove(styles.isDragged)
-      }
-    }
 
-    return undefined
-  }, [wrapperRef, dragNDropContext.dragged, dragNDropContext.draggedElementPosition])
-
+  const isDragged = dragNDropContext.dragged === wrapperRef.current
+  const {draggedElementOriginalPosition, draggedElementPosition} = dragNDropContext
+  const deltaX = draggedElementPosition.x - draggedElementOriginalPosition.x
+  const deltaY = draggedElementPosition.y - draggedElementOriginalPosition.y
   return html`
     <ui-draggable
-      class=${styles.draggable}
+      class=${classnames(styles.draggable, isDragged && styles.isDragged)}
+      style=${isDragged ? `transform: translateX(${deltaX}px) translateY(${deltaY}px)` : null}
       ref=${wrapperRef}
     >
       ${content}
