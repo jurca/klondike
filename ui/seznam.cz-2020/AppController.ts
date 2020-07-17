f1faa8eb5d5a73318a4531b7f68fabc4477e0fd2
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-07-17 13:53:33 +0200

    Move the top-level logic to a controller

---
+++ ui/seznam.cz-2020/AppController.ts
@@ -0,0 +1,211 @@
+import {createElement} from 'react'
+import {render, unmountComponentAtNode} from 'react-dom'
+import {IBotOptions, makeMove} from '../../game/Bot'
+import {ICard, Side} from '../../game/Card'
+import {createNewGame, executeMove, IGame, INewGameRules, redoNextMove, resetGame, undoLastMove} from '../../game/Game'
+import {Move, MoveType} from '../../game/Move'
+import {getMoveHints, HintGeneratorMode} from '../../game/MoveHintGenerator'
+import {deserialize} from '../../game/Serializer'
+import {lastItem, lastItemOrNull} from '../../game/util'
+import App from './App'
+import CardBackfaceStyle from './CardBackfaceStyle'
+import {DESK_SKINS, IDeskSkin} from './deskSkins'
+import SettingsStorage from './storage/SettingsStorage'
+import WinnableGamesProvider from './WinnableGamesProvider'
+
+interface IUIState {
+  game: IGame
+  hint: null | ICard
+  deskSkin: IDeskSkin
+  cardBackFaceStyle: CardBackfaceStyle
+}
+
+export default class AppController {
+  private readonly uiState: Readonly<IUIState>
+
+  constructor(
+    private readonly uiRoot: HTMLElement,
+    private readonly newGameProvider: WinnableGamesProvider,
+    deskSkin: IDeskSkin,
+    cardBackFaceStyle: CardBackfaceStyle,
+    private readonly settingsStorage: SettingsStorage,
+    private readonly newGameOptions: INewGameRules,
+    private readonly botOption: IBotOptions,
+  ) {
+    this.uiState = {
+      cardBackFaceStyle,
+      deskSkin,
+      game: this.createNewGame(1),
+      hint: null,
+    }
+  }
+
+  public start(): void {
+    this.renderUI()
+  }
+
+  public stop(): void {
+    unmountComponentAtNode(this.uiRoot)
+  }
+
+  private updateUI(statePatch: Partial<IUIState>): void {
+    Object.assign(this.uiState, statePatch)
+    this.renderUI()
+  }
+
+  private renderUI(): void {
+    // tslint:disable:object-literal-sort-keys
+    render(
+      createElement(
+        App,
+        {
+          game: this.uiState.game,
+          hint: this.uiState.hint,
+          deskSkin: this.uiState.deskSkin,
+          cardBackFace: this.uiState.cardBackFaceStyle,
+          onMove: this.onMove,
+          onUndo: this.onUndo,
+          onRedo: this.onRedo,
+          onReset: this.onReset,
+          onNewWinnableGame: this.onNewWinnableGame,
+          onShowHint: this.onShowHint,
+          onDeskStyleChange: this.onDeskStyleChange,
+          onCardStyleChange: this.onCardBackStyleChange,
+          onBotMove: this.onBotMove,
+          onImport: this.onImport,
+        },
+      ),
+      this.uiRoot,
+    )
+    // tslint:enable:object-literal-sort-keys
+  }
+
+  private onMove = (move: Move): void => {
+    const statePatch: Partial<IUIState> = {}
+    try {
+      statePatch.game = executeMove(this.uiState.game, move)
+      const unrevealedCardPileIndex = statePatch.game.state.tableau.piles.findIndex(
+        (pile) => lastItemOrNull(pile.cards)?.side === Side.BACK,
+      )
+      if (unrevealedCardPileIndex > -1) {
+        statePatch.game = executeMove(statePatch.game, {
+          move: MoveType.REVEAL_TABLEAU_CARD,
+          pileIndex: unrevealedCardPileIndex,
+        })
+      }
+    } catch (moveError) {
+      // tslint:disable-next-line:no-console
+      console.error(moveError)
+      return
+    }
+
+    statePatch.hint = null
+    this.updateUI(statePatch)
+  }
+
+  private onUndo = (): void => {
+    const statePatch: Partial<IUIState> = {}
+    if (
+      this.uiState.game.history.length &&
+      lastItem(this.uiState.game.history)[1].move === MoveType.REVEAL_TABLEAU_CARD
+    ) {
+      statePatch.game = undoLastMove(this.uiState.game)
+    }
+    statePatch.game = undoLastMove(statePatch.game || this.uiState.game)
+    statePatch.hint = null
+    this.updateUI(statePatch)
+  }
+
+  private onRedo = (): void => {
+    const statePatch: Partial<IUIState> = {}
+    statePatch.game = redoNextMove(this.uiState.game)
+    if (statePatch.game.future.length && statePatch.game.future[0][1].move === MoveType.REVEAL_TABLEAU_CARD) {
+      statePatch.game = redoNextMove(statePatch.game)
+    }
+    statePatch.hint = null
+    this.updateUI(statePatch)
+  }
+
+  private onReset = (): void => {
+    this.updateUI({
+      game: resetGame(this.uiState.game),
+      hint: null,
+    })
+  }
+
+  private onNewWinnableGame = (drawnCards: 1 | 3): void => {
+    const statePatch: Partial<IUIState> = {}
+    statePatch.game = this.createNewGame(drawnCards)
+    statePatch.hint = null
+    this.updateUI(statePatch)
+  }
+
+  private onShowHint = (): void => {
+    if (this.uiState.hint) {
+      this.updateUI({
+        hint: null,
+      })
+      return
+    }
+
+    const {state: gameState, rules} = this.uiState.game
+    const basicHints = getMoveHints(gameState, rules, HintGeneratorMode.CURRENT_STATE)
+    if (basicHints.length) {
+      this.updateUI({
+        hint: basicHints[0][1],
+      })
+    } else {
+      const generalHints = getMoveHints(gameState, rules, HintGeneratorMode.WITH_FULL_STOCK)
+      this.updateUI({
+        hint: generalHints.length ? generalHints[0][1] : null,
+      })
+    }
+  }
+
+  private onDeskStyleChange = (newDeskStyle: string): void => {
+    if (newDeskStyle in DESK_SKINS) {
+      const deskSkin = DESK_SKINS[newDeskStyle as keyof typeof DESK_SKINS]
+      this.settingsStorage.setDeskSkin(deskSkin).catch((error) => {
+        // tslint:disable-next-line:no-console
+        console.error('Failed to save desk skin', error)
+      })
+      this.updateUI({
+        deskSkin,
+      })
+    }
+  }
+
+  private onCardBackStyleChange = (newCardBackStyle: CardBackfaceStyle): void => {
+    this.settingsStorage.setCardBackFaceStyle(newCardBackStyle).catch((error) => {
+      // tslint:disable-next-line:no-console
+      console.error('Failed to save card back face style', error)
+    })
+    this.updateUI({
+      cardBackFaceStyle: newCardBackStyle,
+    })
+  }
+
+  private onBotMove = (): void => {
+    this.updateUI({
+      game: makeMove(this.uiState.game, this.botOption),
+    })
+  }
+
+  private onImport = (): void => {
+    const state = prompt('Exportovaný stav hry:') || ''
+    this.updateUI({
+      game: deserialize(state),
+    })
+  }
+
+  private createNewGame(drawnCards: 1 | 3): IGame {
+    const deck = this.newGameProvider.getWinnableCardDeck(1)
+    return createNewGame(
+      {
+        ...this.newGameOptions,
+        drawnCards,
+      },
+      deck,
+    )
+  }
+}
--- ui/seznam.cz-2020/index.tsx
+++ ui/seznam.cz-2020/index.tsx
@@ -1,148 +1,24 @@
-import * as React from 'react'
-import {render} from 'react-dom'
-import {makeMove} from '../../game/Bot'
-import {ICard, Side} from '../../game/Card'
-import {createNewGame, executeMove, IGame, redoNextMove, resetGame, undoLastMove} from '../../game/Game'
-import {Move, MoveType} from '../../game/Move'
-import {getMoveHints, HintGeneratorMode} from '../../game/MoveHintGenerator'
-import {deserialize} from '../../game/Serializer'
-import {lastItem, lastItemOrNull} from '../../game/util'
-import App from './App'
-import CardBackfaceStyle from './CardBackfaceStyle'
+import AppController from './AppController'
 import {BOT_OPTIONS, DEFAULT_NEW_GAME_OPTIONS} from './config'
-import * as DeskSkins from './deskSkins'
+import primaryStorageFactory from './storage/primaryStorageFactory'
+import SettingsStorage from './storage/SettingsStorage'
 import WinnableGamesProvider from './WinnableGamesProvider'
 
-const uiRoot = document.getElementById('app')!
-
-let game: IGame // TODO: set explicitly to null until a game is generated or selected
-let hint: null | ICard = null
-let deskSkin = DeskSkins.GREEN_S
-let cardBackStyle = CardBackfaceStyle.SeznamLogo
-
-requestAnimationFrame(() => {
-  return onNewWinnableGame(1)
-})
-
-function rerenderUI() {
-  render(
-    <App
-      game={game}
-      hint={hint}
-      deskSkin={deskSkin}
-      cardBackFace={cardBackStyle}
-      onMove={onMove}
-      onUndo={onUndo}
-      onRedo={onRedo}
-      onReset={onReset}
-      onNewWinnableGame={onNewWinnableGame}
-      onShowHint={onShowHint}
-      onDeskStyleChange={onDeskStyleChange}
-      onCardStyleChange={onCardBackStyleChange}
-      onBotMove={onBotMove}
-      onImport={onImport}
-    />,
+const settingsStorage = new SettingsStorage(primaryStorageFactory('settings'))
+Promise.all([
+  settingsStorage.getDeskSkin(),
+  settingsStorage.getCardBackFaceStyle(),
+]).then(([deskSkin, cardBackFaceStyle]) => {
+  const uiRoot = document.getElementById('app')!
+  const winnableGamesProvider = new WinnableGamesProvider()
+  const appController = new AppController(
     uiRoot,
+    winnableGamesProvider,
+    deskSkin,
+    cardBackFaceStyle,
+    settingsStorage,
+    DEFAULT_NEW_GAME_OPTIONS,
+    BOT_OPTIONS,
   )
-}
-
-function onMove(move: Move): void {
-  try {
-    game = executeMove(game, move)
-    const unrevealedCardPileIndex = game.state.tableau.piles.findIndex(
-      (pile) => lastItemOrNull(pile.cards)?.side === Side.BACK,
-    )
-    if (unrevealedCardPileIndex > -1) {
-      game = executeMove(game, {
-        move: MoveType.REVEAL_TABLEAU_CARD,
-        pileIndex: unrevealedCardPileIndex,
-      })
-    }
-  } catch (moveError) {
-    // tslint:disable-next-line:no-console
-    console.error(moveError)
-    return
-  }
-
-  hint = null
-  rerenderUI()
-}
-
-function onNewWinnableGame(drawnCards: 1 | 3): void {
-  game = createWinnableGame(drawnCards)
-  hint = null
-  rerenderUI()
-}
-
-function onUndo(): void {
-  if (game.history.length && lastItem(game.history)[1].move === MoveType.REVEAL_TABLEAU_CARD) {
-    game = undoLastMove(game)
-  }
-  game = undoLastMove(game)
-  hint = null
-  rerenderUI()
-}
-
-function onRedo(): void {
-  game = redoNextMove(game)
-  if (game.future.length && game.future[0][1].move === MoveType.REVEAL_TABLEAU_CARD) {
-    game = redoNextMove(game)
-  }
-  hint = null
-  rerenderUI()
-}
-
-function onReset(): void {
-  game = resetGame(game)
-  hint = null
-  rerenderUI()
-}
-
-function onShowHint(): void {
-  if (hint) {
-    hint = null
-  } else {
-    const basicHints = getMoveHints(game.state, game.rules, HintGeneratorMode.CURRENT_STATE)
-    if (basicHints.length) {
-      hint = basicHints[0][1]
-    } else {
-      const generalHints = getMoveHints(game.state, game.rules, HintGeneratorMode.WITH_FULL_STOCK)
-      hint = generalHints.length ? generalHints[0][1] : null
-    }
-  }
-  rerenderUI()
-}
-
-function onDeskStyleChange(newDeskStyle: string): void {
-  if (newDeskStyle in DeskSkins) {
-    deskSkin = (DeskSkins as any)[newDeskStyle]
-    rerenderUI()
-  }
-}
-
-function onCardBackStyleChange(newCardBackStyle: CardBackfaceStyle): void {
-  cardBackStyle = newCardBackStyle
-  rerenderUI()
-}
-
-function onBotMove() {
-  game = makeMove(game, BOT_OPTIONS)
-  rerenderUI()
-}
-
-function onImport() {
-  const state = prompt('Exportovaný stav hry:') || ''
-  game = deserialize(state)
-  rerenderUI()
-}
-
-const winnableGamesProvider = new WinnableGamesProvider()
-function createWinnableGame(drawnCards: 1 | 3): IGame {
-  return createNewGame(
-    {
-      ...DEFAULT_NEW_GAME_OPTIONS,
-      drawnCards,
-    },
-    winnableGamesProvider.getWinnableCardDeck(drawnCards),
-  )
-}
+  appController.start()
+})
