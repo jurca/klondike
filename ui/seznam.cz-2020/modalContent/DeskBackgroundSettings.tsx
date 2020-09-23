db38f627b318c7d34deed2780a088978205826fa
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-09-23 13:18:21 +0200

    Refactor the deck background settings component - reduce duplicate code

--- ui/seznam.cz-2020/modalContent/DeskBackgroundSettings.tsx
+++ ui/seznam.cz-2020/modalContent/DeskBackgroundSettings.tsx
@@ -5,7 +5,7 @@ import Diamonds from '../deskBackground/diamonds.svg'
 import Hearths from '../deskBackground/hearths.svg'
 import GreenS from '../deskBackground/s.svg'
 import Spades from '../deskBackground/spades.svg'
-import {DESK_SKINS} from '../deskSkins'
+import {DESK_SKINS, IDeskSkin} from '../deskSkins'
 import DeskStyle from '../DeskStyle'
 import {Type} from '../ModalContentHost'
 import styles from './deskBackgroundSettings.css'
@@ -35,59 +35,45 @@ const DeskBackgroundSettings: ModalContentComponent = Object.assign(
       () => (deskStyle: DeskStyle) => React.useMemo(() => themePreviewFactory(deskStyle), [deskStyle]),
       [themePreviewFactory],
     )
+    const themeOptionFactory = React.useMemo(
+      () => (deskSkin: IDeskSkin, ui: React.ReactElement) => ({
+        confirmationUI: cachingThemePreviewFactory(deskSkin.desk.style),
+        isSelected: props.deskStyle === deskSkin.desk.style,
+        ui: (
+          <div className={styles.option} style={{background: deskSkin.desk.background}}>
+            {ui}
+          </div>
+        ),
+      }),
+      [cachingThemePreviewFactory],
+    )
 
     return (
       <ThemeChoice {...props}>
         {[
-          {
-            confirmationUI: cachingThemePreviewFactory(DeskStyle.GREEN_S_TILES),
-            isSelected: props.deskStyle === DeskStyle.GREEN_S_TILES,
-            ui: (
-              <div className={styles.option} style={{background: DESK_SKINS.GREEN_S_TILES.desk.background}}>
-                <div className={deskStyles.greenSTiles}/>
+          themeOptionFactory(DESK_SKINS.GREEN_S_TILES, <div className={deskStyles.greenSTiles}/>),
+          themeOptionFactory(DESK_SKINS.RED_S_TILES, <div className={deskStyles.redSTiles}/>),
+          themeOptionFactory(
+            DESK_SKINS.TEAL_COLORS,
+            <div className={deskStyles.tealColors}>
+              <div className={deskStyles.tealColorsGroup}>
+                <Hearths/>
+                <Spades/>
+                <Diamonds/>
+                <Clubs/>
               </div>
-            ),
-          },
-          {
-            confirmationUI: cachingThemePreviewFactory(DeskStyle.RED_S_TILES),
-            isSelected: props.deskStyle === DeskStyle.RED_S_TILES,
-            ui: (
-              <div className={styles.option} style={{background: DESK_SKINS.RED_S_TILES.desk.background}}>
-                <div className={deskStyles.redSTiles}/>
-              </div>
-            ),
-          },
-          {
-            confirmationUI: cachingThemePreviewFactory(DeskStyle.TEAL_COLORS),
-            isSelected: props.deskStyle === DeskStyle.TEAL_COLORS,
-            ui: (
-              <div className={styles.option} style={{background: DESK_SKINS.TEAL_COLORS.desk.background}}>
-                <div className={deskStyles.tealColors}>
-                  <div className={deskStyles.tealColorsGroup}>
-                    <Hearths/>
-                    <Spades/>
-                    <Diamonds/>
-                    <Clubs/>
-                  </div>
+            </div>,
+          ),
+          themeOptionFactory(
+            DESK_SKINS.GREEN_S,
+            <div className={deskStyles.greenSImageWrapper}>
+              <div className={deskStyles.greenSInnerImageWrapper}>
+                <div className={deskStyles.greenSImage}>
+                  <GreenS/>
                 </div>
               </div>
-            ),
-          },
-          {
-            confirmationUI: cachingThemePreviewFactory(DeskStyle.GREEN_S),
-            isSelected: props.deskStyle === DeskStyle.GREEN_S,
-            ui: (
-              <div className={styles.option} style={{background: DESK_SKINS.GREEN_S.desk.background}}>
-                <div className={deskStyles.greenSImageWrapper}>
-                  <div className={deskStyles.greenSInnerImageWrapper}>
-                    <div className={deskStyles.greenSImage}>
-                      <GreenS/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ),
-          },
+            </div>,
+          ),
         ]}
       </ThemeChoice>
     )
