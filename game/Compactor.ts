bab01288165834d3ce14fc9f632ca0b151e7741f
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-09-29 10:33:59 +0200

    Improve compactor's compression
    Combine multiple stock manipulation moves or undos into single history record.
    Also combine pause and resume into a single history record.

--- game/Compactor.ts
+++ game/Compactor.ts
@@ -1,37 +1,67 @@
 import {DECK, ICard} from './Card'
 import {IDesk} from './Desk'
 import {createNewGame, executeMove, IGame, INewGameRules, undoLastMove} from './Game'
-import {Move} from './Move'
+import {Move, MoveType} from './Move'
 import {lastItem, lastItemOrNull} from './util'
 
 export type CompactCard = Omit<ICard, 'side'>
 
-export type CompactHistoryRecord = Move & {
+export enum CompactMoveType {
+  MULTIPLE_STOCK_MANIPULATIONS = 'CompactMoveType.MULTIPLE_STOCK_MANIPULATIONS',
+  MULTIPLE_UNDO = 'CompactMoveType.MULTIPLE_UNDO',
+  BREAK = 'CompactMoveType.BREAK',
+}
+
+interface IMultipleStockManipulationsMove {
+  readonly move: CompactMoveType.MULTIPLE_STOCK_MANIPULATIONS
+  readonly drawnCards: number
+  readonly timeDeltas: readonly number[]
+}
+
+interface IMultipleUndoMove {
+  readonly move: CompactMoveType.MULTIPLE_UNDO
+  readonly timeDeltas: readonly number[]
+}
+
+interface IBreakMove {
+  readonly move: CompactMoveType.BREAK
+  readonly timeDelta: number
+  readonly duration: number
+}
+
+export type CompactMoveHistoryRecord =
+  IMultipleStockManipulationsMove |
+  IMultipleUndoMove |
+  IBreakMove
+
+export type CompactTimeHistoryRecord = Move & {
   readonly timeDelta: number,
 }
+
+export type CompactHistoryRecord = CompactTimeHistoryRecord | CompactMoveHistoryRecord
 
 export interface ICompactGame {
   readonly deck: readonly CompactCard[]
   readonly rules: INewGameRules,
   readonly startTime: number,
   readonly history: readonly CompactHistoryRecord[]
-  readonly future: readonly CompactHistoryRecord[]
+  readonly future: number
 }
 
 export function compact(game: IGame): ICompactGame {
   const initialState = game.history.length ? game.history[0][0] : game.state
   const cardsDeck = compactDeckFromDesk(initialState)
-  const compactedHistoryRecords = game.history.concat(game.future).map(
+  const compactedHistoryRecords = compactHistoryRecords(game.history.concat(game.future).map(
     (record, index, records) => compactHistoryRecord(
       record,
       records[index - 1]?.[1].logicalTimestamp ?? game.startTime.logicalTimestamp,
     ),
-  )
+  ))
 
   return {
     deck: cardsDeck,
-    future: compactedHistoryRecords.slice(game.history.length),
-    history: compactedHistoryRecords.slice(0, game.history.length),
+    future: game.future.length,
+    history: compactedHistoryRecords,
     rules: {
       ...game.rules,
       tableauPiles: initialState.tableau.piles.length,
@@ -51,23 +81,11 @@ export function compactDeckFromDesk(desk: IDesk): CompactCard[] {
 export function expand(game: ICompactGame): IGame {
   const reconstructedDeck = expandDeck(game.deck)
   const initialState = createNewGame(game.rules, reconstructedDeck)
-  const expandedHistoryRecords: Array<IGame['history'][0][1]> = []
-  for (const record of game.history.concat(game.future)) {
-    expandedHistoryRecords.push(expandHistoryRecord(
-      record,
-      lastItemOrNull(expandedHistoryRecords)?.logicalTimestamp ?? 0,
-    ))
+  const finalState = expandHistoryRecords(initialState, game.history)
+  let expandedGame = finalState
+  for (let i = 0; i < game.future; i++) {
+    expandedGame = undoLastMove(expandedGame)
   }
-
-  const finalState = expandedHistoryRecords.reduce(
-    (gameState: IGame, historyRecord) => {
-      const updatedGame = executeMove(gameState, historyRecord)
-      lastItem(updatedGame.history)[1].logicalTimestamp = historyRecord.logicalTimestamp
-      return updatedGame
-    },
-    initialState,
-  )
-  const expandedGame = game.future.reduce((gameState) => undoLastMove(gameState), finalState)
 
   return {
     ...expandedGame,
@@ -96,7 +114,68 @@ export function expandDeck(deck: readonly CompactCard[]): ICard[] {
   return reconstructedDeck
 }
 
-function compactHistoryRecord(record: IGame['history'][0], previousLogicalTimestamp: number): CompactHistoryRecord {
+function compactHistoryRecords(records: readonly CompactTimeHistoryRecord[]): CompactHistoryRecord[] {
+  const compactedRecords: CompactHistoryRecord[] = []
+  for (let i = 0; i < records.length; i++) {
+    const record = records[i]
+    switch (true) {
+      case [MoveType.REDEAL, MoveType.DRAW_CARDS].includes(record.move):
+        let stockMovesCount = 0
+        let drawnCards: null | number = null
+        for (let j = i; j < records.length && [MoveType.REDEAL, MoveType.DRAW_CARDS].includes(records[j].move); j++) {
+          const stockMove = records[j]
+          if (stockMove.move === MoveType.DRAW_CARDS) {
+            if (drawnCards === null) {
+              drawnCards = stockMove.drawnCards
+            } else if (stockMove.drawnCards !== drawnCards) {
+              break
+            }
+          }
+          stockMovesCount++
+        }
+        if (drawnCards && stockMovesCount > 2) {
+          compactedRecords.push({
+            drawnCards,
+            move: CompactMoveType.MULTIPLE_STOCK_MANIPULATIONS,
+            timeDeltas: records.slice(i, i + stockMovesCount).map((recordToCompact) => recordToCompact.timeDelta),
+          })
+          i += stockMovesCount - 1
+        } else {
+          compactedRecords.push(record)
+        }
+        break
+      case record.move === MoveType.UNDO:
+        let undoCount = 1
+        for (let j = i + 1; j < records.length && records[j].move === MoveType.UNDO; j++) {
+          undoCount++
+        }
+        if (undoCount > 2) {
+          compactedRecords.push({
+            move: CompactMoveType.MULTIPLE_UNDO,
+            timeDeltas: records.slice(i, i + undoCount).map((recordToCompact) => recordToCompact.timeDelta),
+          })
+          i += undoCount - 1
+        } else {
+          compactedRecords.push(record)
+        }
+        break
+      case record.move === MoveType.PAUSE && i < records.length - 1 && records[i + 1].move === MoveType.RESUME:
+        compactedRecords.push({
+          duration: records[i + 1].timeDelta,
+          move: CompactMoveType.BREAK,
+          timeDelta: record.timeDelta,
+        })
+        i++
+        break
+      default:
+        compactedRecords.push(record)
+        break
+    }
+  }
+  return compactedRecords
+}
+
+function compactHistoryRecord(record: IGame['history'][0], previousLogicalTimestamp: number): CompactTimeHistoryRecord {
   const {logicalTimestamp, ...move} = record[1]
   return {
     ...move,
@@ -104,7 +183,70 @@ function compactHistoryRecord(record: IGame['history'][0], previousLogicalTimest
   }
 }
 
-function expandHistoryRecord(record: CompactHistoryRecord, previousLogicalTimestamp: number): IGame['history'][0][1] {
+function expandHistoryRecords(
+  initialState: IGame,
+  records: readonly CompactHistoryRecord[],
+): IGame {
+  const expandedRecords: CompactTimeHistoryRecord[] = []
+  for (const record of records) {
+    switch (record.move) {
+      case CompactMoveType.MULTIPLE_STOCK_MANIPULATIONS:
+        expandedRecords.push(...record.timeDeltas.map<CompactTimeHistoryRecord>((timeDelta) => ({
+          drawnCards: record.drawnCards,
+          move: MoveType.DRAW_CARDS, // Will be changed to REDEAL where needed when replaying history of the game
+          timeDelta,
+        })))
+        break
+      case CompactMoveType.MULTIPLE_UNDO:
+        expandedRecords.push(...record.timeDeltas.map<CompactTimeHistoryRecord>((timeDelta) => ({
+          move: MoveType.UNDO,
+          timeDelta,
+        })))
+        break
+      case CompactMoveType.BREAK:
+        expandedRecords.push(
+          {
+            move: MoveType.PAUSE,
+            timeDelta: record.timeDelta,
+          },
+          {
+            move: MoveType.RESUME,
+            timeDelta: record.duration,
+          },
+        )
+        break
+      default:
+        expandedRecords.push(record)
+        break
+    }
+  }
+
+  const historyRecords: Array<IGame['history'][0][1]> = []
+  for (const record of expandedRecords) {
+    historyRecords.push(expandHistoryRecord(
+      record,
+      lastItemOrNull(historyRecords)?.logicalTimestamp ?? 0,
+    ))
+  }
+
+  let game = initialState
+  for (const record of historyRecords) {
+    if (record.move === MoveType.DRAW_CARDS && !game.state.stock.cards.length) {
+      game = executeMove(game, {
+        move: MoveType.REDEAL,
+      })
+    } else {
+      game = executeMove(game, record)
+    }
+    lastItem(game.history)[1].logicalTimestamp = record.logicalTimestamp
+  }
+  return game
+}
+
+function expandHistoryRecord(
+  record: CompactTimeHistoryRecord,
+  previousLogicalTimestamp: number,
+): IGame['history'][0][1] {
   const {timeDelta, ...move} = record
   return {
     ...move,
