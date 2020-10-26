d4a4889e49c4a07b022d26d604d2b6ee0a4b5acc
Martin Jurča <martin.jurca@firma.seznam.cz>
2020-10-26 07:01:22 +0100

    Include an actual "how to play" text

--- ui/seznam.cz-2020/modalContent/HowToPlay.tsx
+++ ui/seznam.cz-2020/modalContent/HowToPlay.tsx
@@ -9,49 +9,38 @@ const HowToPlay: ModalContentComponent = Object.assign(function HowToPlayUI(prop
     <ModalContentWithBottomCloseButton {...props}>
       <div className={styles.howToPlay}>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at vestibulum ligula. Suspendisse vitae
-          tincidunt sem. Aenean pharetra neque at nunc commodo consectetur. Pellentesque habitant morbi tristique
-          senectus et netus et malesuada fames ac turpis egestas. Nulla condimentum ante eget augue tincidunt, vitae
-          posuere magna rutrum. Aliquam ac mollis sem. Donec sed molestie quam. Vestibulum et sollicitudin augue. Etiam
-          sit amet posuere nunc. Donec malesuada a eros et ultricies. Donec id faucibus est, id viverra dui. Fusce
-          mattis nibh sed imperdiet euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-          inceptos himenaeos. Sed mollis varius lobortis. Fusce viverra interdum egestas. Donec sollicitudin id arcu in
-          dictum.
+          Cílem hry je přesouváním karet z&nbsp;tahacího balíčku a herních sloupců vytvořit v&nbsp;odkládacích balíčkách
+          čtyři postupky tvořené kartami stejné barvy seřazenými vzestupně od Esa (A) po Krále (K).
         </p>
+        <h2>
+          Rozložení karet na hrací ploše
+        </h2>
+        <h3>
+          Tahací balíček (vlevo nahoře)
+        </h3>
         <p>
-          Praesent auctor bibendum elit, id pharetra lorem rhoncus in. Fusce id dignissim ante, sit amet semper orci.
-          Nulla convallis vitae nibh a fermentum. Nam sit amet iaculis velit, quis mattis ligula. Aenean eget magna
-          sapien. Vivamus eget ex sollicitudin, condimentum sem eu, condimentum arcu. Mauris tellus nulla, molestie eu
-          rutrum ut, blandit semper dui. Morbi nec neque id dolor accumsan pulvinar. Donec quis erat sit amet ligula
-          eleifend suscipit eget vel nisl. Sed porta varius metus, vel rutrum risus. Vestibulum id finibus ligula, eget
-          tempor metus. Vivamus vitae pretium augue, facilisis tempor risus. Cras ut nulla est.
+          Podle toho, jakou jste na začátku hry zvolili obtížnost, si z&nbsp;něj kliknutím odkryjete jednu nebo tři
+          karty. Horní odkrytou kartu můžete přesunout do jiné části herní plochy nebo ji ponechat v&nbsp;tahacím
+          balíčku. Dokud v&nbsp;něm zbývají nepoužité karty, lze balíček procházet opakovaně.
         </p>
+        <h3>
+          Odkládací balíčky (vpravo nahoře)
+        </h3>
         <p>
-          Nulla est mi, vulputate sit amet molestie eget, dapibus sed elit. Phasellus laoreet laoreet nulla id rhoncus.
-          Pellentesque quis nibh et turpis volutpat rhoncus vel ut mauris. Fusce semper diam quis nunc fringilla
-          rhoncus. Integer luctus risus eget nibh tincidunt accumsan. Suspendisse nisl neque, varius eu ante facilisis,
-          vulputate gravida mi. Integer sit amet turpis sed orci pharetra tincidunt ac ac tortor. Praesent gravida vitae
-          risus vel ultrices. Pellentesque sagittis ipsum lorem, eget finibus sapien tempus ut. Duis aliquet vel odio
-          eget posuere. Fusce in nibh quis dui egestas euismod in eget leo. Nullam eget nulla quam. Praesent interdum,
-          sapien sit amet sodales finibus, erat dolor egestas est, nec consectetur lacus mauris eget velit. Integer
-          molestie turpis et lorem ultricies fringilla at nec libero. Nunc imperdiet sapien non accumsan commodo.
+          Do odkládacích balíčků přesuňte odkrytá Esa a na každé z&nbsp;nich postupně další karty stejné barvy seřazené
+          postupně podle hodnoty od nejnižších po nejvyšší (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K). Horní kartu
+          z&nbsp;každého odkládacího balíčku můžete vzít a umístit do některého herního sloupce.
         </p>
+        <h3>
+          Herní sloupce
+        </h3>
         <p>
-          Phasellus eget augue mattis, ullamcorper massa eu, ornare lacus. Sed in eros risus. Praesent egestas velit ut
-          dui finibus varius. Vestibulum vel mauris quis nulla porta consequat sit amet a tortor. Maecenas laoreet ante
-          nec lectus malesuada faucibus. Sed dapibus est eu sagittis malesuada. Mauris nisi magna, consectetur sit amet
-          sem ut, tempus consequat dui. Curabitur auctor nulla et dui fringilla faucibus. Phasellus fringilla nisl in
-          pretium cursus. Etiam tristique lorem nec dui pretium, at pretium leo egestas. Nunc eu leo dignissim, sagittis
-          eros eget, finibus lorem. Phasellus pellentesque leo tristique convallis semper. Proin lacinia lorem non nisl
-          posuere, ac suscipit diam volutpat. Fusce enim lacus, maximus sit amet elit eu, tempus laoreet orci.
-        </p>
-        <p>
-          Integer commodo malesuada nisi, quis ullamcorper magna mollis sed. Pellentesque at nibh et diam convallis
-          interdum vel ut libero. Nam egestas in diam vitae eleifend. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos. Praesent sed fringilla est. Vivamus faucibus auctor porttitor.
-          Nunc accumsan diam sed euismod fringilla. Donec fermentum risus eu elit dignissim vehicula. Nam placerat
-          mauris dui, quis pharetra diam tristique nec. Vivamus eleifend ornare metus et auctor. Integer et velit
-          maximus, laoreet neque et, varius orci. Praesent scelerisque hendrerit quam, eu accumsan magna finibus a.
+          Každý herní sloupec má na začátku hry odkrytou jednu kartu. Na odkryté karty umisťujte z&nbsp;tahacího balíčku
+          nebo z&nbsp;jiných herních sloupců karty s hodnotou o jedna menší a s&nbsp;opačnou barvou (např. na červenou
+          osmičku můžete umístit pouze černou sedmičku). Odkryté karty lze mezi sloupci přemisťovat po jedné i po
+          skupinách, skupiny seřazených karet je možné rozpojovat. Když ze sloupce odeberete poslední odkrytou kartu,
+          automaticky se odkryje karta pod ní. Pokud všechny karty ze sloupce přesunete jinam, můžete na jeho místo
+          umístit Krále nebo skupinu karet, jejíž nejvyšší kartou je Král.
         </p>
       </div>
     </ModalContentWithBottomCloseButton>
